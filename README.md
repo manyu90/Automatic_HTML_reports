e74b5b8def5b38b9809bc5855089659b68889b0f
manyu90 <abhimanyusid@gmail.com>
2019-04-22 23:09:40 -0700

    Cleanup

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Automatic_HTML_reports
+# Automatic_HTML_reports using Jinja2
+
+##Use Jinja2 templating to auto generate a static site given a template. This demonstrates the general layout
+
+All templates and html that is embedded must be kept in the templates folder. The template file is usually called *index.html* . There is also a .html table called df_info.html which is insered into the template (I generated the table from a pandas dataframe (convert to html feature of the df))
+
+THe paths to the figures and variables can be passed as a dictionary to the template renderer. 
+
+To generate the .html static page run the following:
+```python generate_from_filesystem.py```
+
+This would create the .html rendered page in the \html folder
--- generate_from_filesystem.py
+++ generate_from_filesystem.py
@@ -8,10 +8,9 @@ template = env.get_template('index.html')
  
  
 filename = os.path.join(root, 'html', 'motifs.html')
-variables = {'motifs':'/Users/abhimanyu/Dropbox/Documents/Research/DL_for_Genomics/C2H2_ZNF/html_reports/example_figures/plot_motifs.jpg',
-			'clustermap':'/Users/abhimanyu/Dropbox/Documents/Research/DL_for_Genomics/C2H2_ZNF/html_reports/example_figures/clustermap_long_patterns.jpg',
+variables = {'motifs':os.path.join(root,'plot_motifs.jpg'),
+			'clustermap':os.path.join(root,'clustermap_long_patterns.jpg'),
 			'TF':'Any Old TF: ZNF273'}
-#motifs_path = '/Users/abhimanyu/Dropbox/Documents/Research/DL_for_Genomics/C2H2_ZNF/html_reports/example_figures/plot_motifs.jpg'
 with open(filename, 'w') as fh:
     fh.write(template.render(vars = variables))
 
