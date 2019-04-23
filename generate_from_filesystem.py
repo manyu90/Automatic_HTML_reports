3f4efd9df35aece6eae5cea477206e243a37b7c1
manyu90 <abhimanyusid@gmail.com>
2019-04-23 13:59:40 -0700

    updates to template and figs

--- generate_from_filesystem.py
+++ generate_from_filesystem.py
@@ -10,7 +10,8 @@ template = env.get_template('index.html')
 filename = os.path.join(root, 'html', 'motifs.html')
 variables = {'motifs':os.path.join(root,'plot_motifs.jpg'),
 			'clustermap':os.path.join(root,'clustermap_long_patterns.jpg'),
-			'TF':'Any Old TF: ZNF273'}
+			'TF':'Any Old TF: ZNF273',
+			'meme':os.path.join(root,'meme_motif.jpg')}
 with open(filename, 'w') as fh:
     fh.write(template.render(vars = variables))
 
